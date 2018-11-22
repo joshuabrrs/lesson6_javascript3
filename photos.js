29834220d0387a65fc4feae222f5d91c665f64c1
Joshua Barrios <jgb16@my.fsu.edu>
2018-11-22 03:23:55 +0000

    final commit

--- photos.js
+++ photos.js
@@ -1,8 +1,8 @@
 
 /*    
     Program Name:  Photo Gallery Application
-    Author: 
-    Date:   
+    Author: Joshua Barrios  
+    Date:   11/21/2018
     Filename: photos.js
     
  */
@@ -18,9 +18,23 @@ var figureCount = 3;
 /* This method adds src values to img elements based on order specified in photoOrder array             */
 /********************************************************************************************************/
 function populateFigures() {
-
-
-
+var filename;
+var currentFig;
+if (figureCount === 3){
+for (var i = 1;  i < 4;  i++) {
+    filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+    currentFig = document.getElementsByTagName("img")[i -1];
+    currentFig.src = filename;
+}
+    }  else {
+        for (var i = 0; i < 5; i++) {
+            filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+            currentFig = document.getElementsByTagName("img")[i];
+            currentFig.src = filename;
+            
+        }
+        
+    }//end of else
 
 }//end of populateFigures function
 
@@ -69,10 +83,34 @@ function leftArrow() {
 /*           This method is executed to allow the photo gallery to shift to five image layout.         */
 /******************************************************************************************************/
 function previewFive() {
+//locate first element where the tag is article assign to a variable
+    
+var articleEl = document.getElementsByTagName("article")[0];
    
+var lastFigure = document.createElement("figure");
+lastFigure.id = "fig5";
+lastFigure.style.zIndex = "5";
+lastFigure.style.position = "absolute";
+lastFigure.style.right = "45px";
+lastFigure.style.top = "67px";
 
-   
-   
+var lastImage = document.createElement("img");
+lastImage.width = "240";
+lastImage.height = "135";
+lastFigure.appendChild(lastImage);
+articleEl.appendChild(lastFigure);
+var firstFigure = lastFigure.cloneNode(true);
+firstFigure.id = "fig1";
+firstFigure.style.right = " ";
+firstFigure.style.left = "45px";
+
+articleEl.insertBefore(firstFigure, document.getElementById("fig2"));
+document.getElementsByTagName("img")[0].src = "images/IMG_0" + photoOrder[0] + "sm.jpg";
+document.getElementsByTagName("img")[4].src = "images/IMG_0" + photoOrder[4] + "sm.jpg";
+figureCount = 5;
+//disable the ‘Show more images’ button after it has been selected once
+var numberButton = document.querySelector("#fiveButton p");
+numberButton.removeEventListener("click", previewFive, false);
 }//end of previewFive()
 
 /****************************************END of previewFive() Function**********************************/
@@ -83,10 +121,24 @@ function previewFive() {
 /*         This method attaches event listeners to the buttons (left, right, and show all).            */
 /*******************************************************************************************************/
 function createEventListeners() {
+    
    
+//declare variable for document element with id of leftarrow, which is in the html document
+var leftarrow = document.getElementById("leftarrow");
+leftarrow.addEventListener("click", leftArrow, false);
+//declare variable for document element with id of rightarrowin the html document
+var rightarrow = document.getElementById("rightarrow");
+
+//the rightArrow() function will executed when user clicks the right navigation arrow 
+rightarrow.addEventListener("click", rightArrow, false);
 
 
-   
+var showAllButton = document.querySelector("#fiveButton p");
+//add event listener to showAllButton element
+//when the user clicks the Show More button the previewFive function is executed
+showAllButton.addEventListener("click", previewFive, false);
+ var showAllButton = document.querySelector("#fiveButton p");
+ showAllButton.addEventListener("click", previewFive, false);
 }//end of createEventListeners function
 
 /************************************END OF createEventListeners() FUNCTION****************************/
